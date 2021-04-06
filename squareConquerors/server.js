b2082f1acb2d81efd914614a56753b27a4b62355
Sandra Vázquez <svazquezcan@uoc.edu>
2021-04-06 23:12:55 +0200

    Login y Register enrutados y maquetación hecha

--- squareConquerors/server.js
+++ squareConquerors/server.js
@@ -1,30 +1,91 @@
 const http = require('http');
+const fs = require('fs');
 const url = require('url');
+let path = require('path');
 const port = 8000; 
-const host = 'localhost'
+const host = 'localhost';
 
 // Crear nuestro servidor
 const server = http.createServer((req, res) => {
-    //Depuracion
-    console.log("req.url", req.url);
-    console.log("url.parse", url.parse(req.url));
-    //Path
-    const path = req.url;  
+
+    //Url
+    let url = req.url;  
+    console.log("url", url)
+    
 
     //Enrutado
-    if (path === '/'){
-        res.statusCode = 200;
-        res.setHeader = ("Content-type", "text-plain");
-        res.end("Hello World");
-      
-    } else {
-        res.writeHead(404); 
-        res.end('PAGE NOT FOUND');
+    if (url === '/' || url === '/login.html'){
+        url = "/login.html"
+        url = `${__dirname}/public`+url; 
+        fs.readFile(url, 'utf8', (err, data) =>{
+            if (err){
+                res.writeHead(404); 
+                res.end('PAGE NOT FOUND');
+            } else {
+                res.writeHead(200, {"Content-Type": "text/html"});
+                res.end(data);
+            }
+        });
     }
 
-    
+    else if(url === '/register.html'){
+        url = `${__dirname}/public`+url; 
+        fs.readFile(url, 'utf8', (err, data) =>{
+            if (err){
+                res.writeHead(404); 
+                res.end('PAGE NOT FOUND');
+            } else {
+                res.writeHead(200, {"Content-Type": "text/html"});
+                res.end(data);
+            }
+        });
+    }
+
+    else if(url.includes('styles')){
+        url = `${__dirname}`+url;
+        fs.readFile(url, 'utf8', (err, data) =>{
+            if (err){
+                res.writeHead(404); 
+                res.end('PAGE NOT FOUND');
+            } else {
+                res.writeHead(200, {"Content-Type": "text/css"});
+                res.end(data);
+            }
+
+        });
+
+    }
+
+    else if(url.includes('images')){
+        url = `${__dirname}`+url;
+        fs.readFile(url, (err, data) =>{
+            if (err){
+                res.writeHead(404); 
+                res.end('PAGE NOT FOUND');
+            } else {
+                res.writeHead(200, {"Content-Type": "image/png"});
+                res.end(data);
+            }
+
+        });
+    }
+
+    else if(path.extname === '.js'){
+        url = `${__dirname}`+url;
+        fs.readFile(url, (err, data) =>{
+            if (err){
+                res.writeHead(404); 
+                res.end('PAGE NOT FOUND');
+            } else {
+                res.writeHead(200, {"Content-Type": "text/javascript"});
+                res.end(data);
+            }
+
+        });
+    }
+        
 });
-    
+
 //Lanzamos el servidor de forma asincrona
 server.listen(port, host,() =>{
     console.log(`Server is running on http://${host}:${port}`);
